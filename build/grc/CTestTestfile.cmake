3739d41dba64f9e8f20c81c39b49ddf06228bb49
Santiago Castro Guzmán <santiagomcg@gmail.com>
2017-10-18 20:57:32 -0300

    Se edito un comentario en square_ff_impl.cc
    Se agrego una linea comentada "Esta es una linea de prueba"

---
+++ build/grc/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/santiago/gr-howto/grc
+# Build directory: /home/santiago/gr-howto/build/grc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/python/qa_square_ff_test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+export VOLK_GENERIC=1
+export GR_DONT_LOAD_PREFS=1
+export srcdir=/home/santiago/gr-howto/python
+export PATH=/home/santiago/gr-howto/build/python:$PATH
+export LD_LIBRARY_PATH=/home/santiago/gr-howto/build/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=/home/santiago/gr-howto/build/swig:$PYTHONPATH
+/usr/bin/python2 /home/santiago/gr-howto/python/qa_square_ff.py 
